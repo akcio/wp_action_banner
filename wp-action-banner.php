16aa3fdaca892b124c89ee18f9896d00945c6e72
Akcio <mail@test.mail>
2020-05-07 19:55:07 +0500

    test 4

--- wp-action-banner.php
+++ wp-action-banner.php
@@ -154,7 +154,41 @@ function my_custom_init(){
         'menu_position'      => null,
         'supports'           => array('custom-fields','title', 'editor', 'thumbnail')
     ) );
-    register_taxonomy("Skills", array("book"), array("hierarchical" => true, "label" => "Skills", "singular_label" => "Skill", "rewrite" => true));
+    add_action('init', 'portfolio_register');
+
+    function portfolio_register() {
+
+        $labels = array(
+            'name' => _x('My Portfolio', 'post type general name'),
+            'singular_name' => _x('Portfolio Item', 'post type singular name'),
+            'add_new' => _x('Add New', 'portfolio item'),
+            'add_new_item' => __('Add New Portfolio Item'),
+            'edit_item' => __('Edit Portfolio Item'),
+            'new_item' => __('New Portfolio Item'),
+            'view_item' => __('View Portfolio Item'),
+            'search_items' => __('Search Portfolio'),
+            'not_found' =>  __('Nothing found'),
+            'not_found_in_trash' => __('Nothing found in Trash'),
+            'parent_item_colon' => ''
+        );
+
+        $args = array(
+            'labels' => $labels,
+            'public' => true,
+            'publicly_queryable' => true,
+            'show_ui' => true,
+            'query_var' => true,
+            'menu_icon' => get_stylesheet_directory_uri() . '/article16.png',
+            'rewrite' => true,
+            'capability_type' => 'post',
+            'hierarchical' => false,
+            'menu_position' => null,
+            'supports' => array('title','editor','thumbnail')
+        );
+
+        register_post_type( 'portfolio' , $args );
+    }
+    register_taxonomy("Skills", array("book", "portfolio"), array("hierarchical" => true, "label" => "Skills", "singular_label" => "Skill", "rewrite" => true));
 }
 add_action('init', 'my_custom_init');
 
