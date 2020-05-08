976f978f01a968c9783c1171b5e09665668ccf64
Akcio <mail@test.mail>
2020-05-08 18:34:33 +0500

    Some 1

--- wp-action-banner.php
+++ wp-action-banner.php
@@ -274,6 +274,8 @@ if (!function_exists('slides_meta')) {
         global $post;
         $custom = get_post_custom($post->ID);
         $slides = $custom["slides"][0];
+        print_r($slides);
+        exit(1);
         if (empty($slides)) {
             $slides = Array('items' => Array());
         }
@@ -387,6 +389,32 @@ if (!function_exists('slides_meta')) {
             });
         </script>
         <?php
+    }
+}
+
+if ( ! function_exists( 'sanitize_text_or_array_field' ) ) {
+    /**
+     * Recursive sanitation for text or array
+     *
+     * @param $array_or_string (array|string)
+     * @return mixed
+     * @since  0.1
+     */
+    function sanitize_text_or_array_field($array_or_string)
+    {
+        if (is_string($array_or_string)) {
+            $array_or_string = sanitize_text_field($array_or_string);
+        } elseif (is_array($array_or_string)) {
+            foreach ($array_or_string as $key => &$value) {
+                if (is_array($value)) {
+                    $value = sanitize_text_or_array_field($value);
+                } else {
+                    $value = sanitize_text_field($value);
+                }
+            }
+        }
+
+        return $array_or_string;
     }
 }
 
