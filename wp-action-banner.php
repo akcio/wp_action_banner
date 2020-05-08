43161a2f4c10b7b7cd4fe20a3fad2d908c1dbfd2
Akcio <mail@test.mail>
2020-05-08 15:13:16 +0500

    Test html form 2

--- wp-action-banner.php
+++ wp-action-banner.php
@@ -382,6 +382,10 @@ if (!function_exists('slides_meta')) {
 if (!function_exists('save_action_stickers_meta')) {
     function save_action_stickers_meta($post_id)
     {
+        $post_type = get_post_type( $post_id );
+        if ($post_type != 'action_banner') {
+            return;
+        }
         print_r($_POST);
         exit(1);
     }
