d7eac430170c18df55750670d48cc4af7563b3ca
Chizhov Igor <ap-inc@mail.ru>
2020-05-20 09:09:40 +0500

    template6

--- assets/js/ab-scripts.js
+++ assets/js/ab-scripts.js
@@ -1,12 +1,14 @@
 jQuery(document).ready(function( $ ) {
-	$(".action-banner > div:gt(0)").hide();
+	if ($(".action-banner").children().length > 1) {
+		$(".action-banner > div:gt(0)").hide();
 
-	setInterval(function() { 
-	  $('.action-banner > div:first')
-	    .fadeOut(500)
-	    .next()
-	    .fadeIn(500)
-	    .end()
-	    .appendTo('.action-banner');
-	},  5000);
+		setInterval(function() { 
+		  $('.action-banner > div:first')
+		    .fadeOut(500)
+		    .next()
+		    .fadeIn(500)
+		    .end()
+		    .appendTo('.action-banner');
+		},  5000);
+	}
 });
--- wp-action-banner.php
+++ wp-action-banner.php
@@ -413,6 +413,11 @@ if (!function_exists('action_banner_shortcode')) {
         } else {
             $slides = json_decode($slides, true);
         }
+
+        $out = '<style>
+
+                </style>';
+
         $out = '<div class="action-banner">';
         foreach ($slides['items'] as $slide) {
             $img = $slide['image'];
@@ -420,27 +425,29 @@ if (!function_exists('action_banner_shortcode')) {
             $text = $slide['text'];
             $buttons = $slide['buttons'];
             $out .= '
-                <div class="slide" style="background-image: url(' . $img . ');">
-                	<div class="ab-wrapper">
-                        <div class="ab-header">' . $header . '</div>
-                        <div class="ab-text">' . $text . '</div>
-                        <div class="ab-buttons">';
-                        foreach ($buttons as $name => $link) {
-                            $out .= '<button onclick="document.location=\'' . $link . '\'">' . $name . '</button>';
-                        }
-                        $out .= '
+                <div class="ab-slide" style="background-image: url(' . $img . ');">
+                    <div class="ab-blackout">
+                    	<div class="ab-wrapper">
+                            <div class="ab-header">' . $header . '</div>
+                            <div class="ab-text">' . $text . '</div>
+                            <div class="ab-buttons">';
+                            foreach ($buttons as $name => $link) {
+                                $out .= '<button onclick="document.location=\'' . $link . '\'">' . $name . '</button>';
+                            }
+                            $out .= '
+                            </div>
                         </div>
                     </div>
                 </div>';
         }
 
-        /* Only for test */
-        $out .= '
+        /* Only for test! */
+        /*$out .= '
             <div class="slide">
                 <div class="ab-wrapper">
                     TEST
                 </div>
-            </div>';
+            </div>';*/
 
         $out .= '</div>';
         return $out;
